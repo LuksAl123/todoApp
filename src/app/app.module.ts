19c63b8a0a98c7db7901a9d9e75a3006533c6e01
LuksAl123 <lucasalprogrammer@gmail.com>
2024-11-28 16:11:08 -0300

    fifth commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
+@NgModule({
+  declarations: [AppComponent, CreateTaskComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule,
+    FormsModule, IonicStorageModule.forRoot()],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {}
---
+++ src/app/components/create-task/create-task.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-task',
+  templateUrl: './create-task.component.html',
+  styleUrls: ['./create-task.component.scss'],
+})
+
+export class CreateTaskComponent implements OnInit {
+  priority = ['High', 'Medium', 'Low'];
+
+  todoForm = new FormGroup({
+    Title: new FormControl(''),
+    Description: new FormControl(''),
+    Priority: new FormControl('priority'),
+    setDate: new FormControl(new Date().toISOString()),
+    startTime: new FormControl(new Date().toISOString()),
+    endTime: new FormControl(new Date().toISOString()),
+  });
+
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit() {}
+
+  dismissModal() {
+    this.modalCtrl.dismiss();
+  }
+
+  onSubmit() {
+    if (this.todoForm.valid) {
+      console.log('Form Submitted:', this.todoForm.value);
+      this.dismissModal();
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
+
