f9e0450a066e52223166c4ae84ac4c224f708f92
LuksAl123 <lucasalprogrammer@gmail.com>
2024-11-29 14:33:40 -0300

    sixth commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,7 +13,6 @@ export class HomePage {
   constructor(private modalCtrl : ModalController) {}
 
   async addTask(){
-    debugger
     const modal = await this.modalCtrl.create({
       component: CreateTaskComponent,
       showBackdrop: true,
